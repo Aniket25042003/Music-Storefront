3dda3bc249c2a0ffc7f5a096bf414a3ea09da095
Aniket25042003 <aniketg2003patel@gmail.com>
2024-12-10 11:58:49 -0500

    Added js file

---
+++ assets/js/contactjava.js
@@ -0,0 +1,6 @@
+function ValidateEmail()
+{
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    alert("Thanks for filling out our form " + fname + " " + lname + ". A member of our team will be in contact shortly" + "!");
+}
